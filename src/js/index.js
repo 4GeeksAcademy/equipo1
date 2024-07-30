7ae381605295b2e83d3ee29d18f1d27dc1881413
Juan Carlos Díaz <169100845+Nxusbets@users.noreply.github.com>
2024-07-29 21:22:47 -0600

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// include your styles into the webpack bundle
+import "../styles/index.css";
+
+//import your own components
+import Home from "./component/home.jsx";
+
+//render your react application
+ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+
