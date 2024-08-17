3ea45601506262759bed527f3193d73831a5f7f3
Pablo Dávila <167660504+PABLIUXDEV@users.noreply.github.com>
2024-08-17 01:11:44 +0000

    jumbotron pablo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Jumbotron from "./jumbotron.jsx"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +7,8 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Jumbotron/>
 		</div>
 	);
 };
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Jumbotron = () => {
+    return <div className="p-5 mb-4 bg-light rounded-3">
+    <div className="container-fluid py-5">
+      <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
+      <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus, libero ut varius cursus, arcu tortor aliquam urna, in sagittis nulla nulla ac magna. Duis venenatis lectus augue, non sodales sapien mattis ut. Sed dui est, faucibus sit amet iaculis nec, tristique non risus. In nec commodo urna, ut pellentesque metus.</p>
+      <button className="btn btn-primary btn-lg" type="button">Call to action</button>
+    </div>
+  </div>
+}
+
+export default Jumbotron;
